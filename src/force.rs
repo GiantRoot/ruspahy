ce78ea0c4da485085cfc586e67145411b0e8bf68
GiantRoot <zhangjunfan@live.cn>
2025-05-26 12:41:41 +0800

    feat: introduce material system and parallel loops

--- src/force.rs
+++ src/force.rs
@@ -6,58 +6,64 @@
 
 use crate::particle::{Particle, ParticleSystem};
 use crate::sph_kernel::SPHKernel;
+use rayon::prelude::*;
 
 /// Compute density and pressure for every particle.
 pub fn compute_density_pressure(psys: &mut ParticleSystem, kernel: &SPHKernel) {
-    let particles = &mut psys.particles;
-    let mass = 1.0; // assume unit mass
+    let all_particles = psys.particles.clone();
+    let mass = 1.0;
 
-    for i in 0..particles.len() {
-        let mut density = 0.0;
-        let pi = &particles[i];
-        for j in 0..particles.len() {
-            let pj = &particles[j];
-            let r2 = squared_distance(pi.position, pj.position);
-            density += mass * kernel.w_poly6(r2);
-        }
-        particles[i].density = density;
-        particles[i].pressure = 1000.0 * (density - 1000.0); // EOS: p = k(rho - rho0)
-    }
+    psys
+        .particles
+        .par_iter_mut()
+        .for_each(|pi| {
+            let mut density = 0.0;
+            for pj in &all_particles {
+                let r2 = squared_distance(pi.position, pj.position);
+                density += mass * kernel.w_poly6(r2);
+            }
+            pi.density = density;
+            pi.pressure = 1000.0 * (density - 1000.0);
+        });
 }
 
 /// Compute pressure and viscosity forces.
 pub fn compute_forces(psys: &mut ParticleSystem, kernel: &SPHKernel) {
-    let particles = &mut psys.particles;
+    let all_particles = psys.particles.clone();
     let mass = 1.0;
     let viscosity = 0.1;
 
-    for i in 0..particles.len() {
-        let mut force = [0.0; 3];
-        let pi = &particles[i];
-        for j in 0..particles.len() {
-            if i == j { continue; }
-            let pj = &particles[j];
+    psys
+        .particles
+        .par_iter_mut()
+        .enumerate()
+        .for_each(|(i, pi)| {
+            let mut force = [0.0; 3];
+            for (j, pj) in all_particles.iter().enumerate() {
+                if i == j {
+                    continue;
+                }
 
-            let r_vec = vector_sub(pj.position, pi.position);
-            let r2 = dot(r_vec, r_vec);
-            let r = r2.sqrt();
+                let r_vec = vector_sub(pj.position, pi.position);
+                let r2 = dot(r_vec, r_vec);
+                let r = r2.sqrt();
 
-            // pressure force
-            let grad_w = kernel.grad_w_spiky(r, r_vec);
-            let pressure_term = (pi.pressure + pj.pressure) / (2.0 * pj.density);
-            for k in 0..3 {
-                force[k] -= mass * pressure_term * grad_w[k];
+                // pressure force
+                let grad_w = kernel.grad_w_spiky(r, r_vec);
+                let pressure_term = (pi.pressure + pj.pressure) / (2.0 * pj.density);
+                for k in 0..3 {
+                    force[k] -= mass * pressure_term * grad_w[k];
+                }
+
+                // viscosity force
+                let vel_diff = vector_sub(pj.velocity, pi.velocity);
+                let lap_w = kernel.lap_w_viscosity(r);
+                for k in 0..3 {
+                    force[k] += viscosity * mass * vel_diff[k] / pj.density * lap_w;
+                }
             }
-
-            // viscosity force
-            let vel_diff = vector_sub(pj.velocity, pi.velocity);
-            let lap_w = kernel.lap_w_viscosity(r);
-            for k in 0..3 {
-                force[k] += viscosity * mass * vel_diff[k] / pj.density * lap_w;
-            }
-        }
-        particles[i].force = force;
-    }
+            pi.force = force;
+        });
 }
 
 /// Utility: squared distance between two 3D points.
